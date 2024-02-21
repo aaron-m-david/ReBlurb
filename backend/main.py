20e02006e993bf045bdcef44eec8b1448a6d79c0
Aaron David <amdavid@csuchico.edu>
2024-02-20 18:24:27 -0800

    add ability to dynamically parse strings as input

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,14 @@ import os
 
 from flask import Flask, request, jsonify
 from openai import OpenAI
+from typing import List
+# GPT-3.5-turbo-0125 can use 16,000 tokens in a single request
+MAX_TOKENS=16000
+# Save 250 tokens at least to get back response
+TOKENS_TO_USE=MAX_TOKENS - 250
+# Rough estimate of max chars to use in request given we reserve 250 tokens for response
+MAX_CHARS=4 * TOKENS_TO_USE
+
 
 app = Flask(__name__)
 
@@ -14,23 +22,36 @@ def process_openAiKey():
     reviews = data["reviews"] if "reviews" else None
     if (reviews == None):
         return jsonify({'error': 'Missing required data field: reviews'})
-
+    review_string = create_user_content(reviews)
     # Init our openAI client
     client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY')) # Use secret var to store API KEY
     # Query gpt
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        # gpt-3.5-turbo-0125 supports 16k tokens, cheapest, effective
+        model="gpt-3.5-turbo-0125",
         # User content is hardcoded for now, should be dynamic based on what client sends in future
         messages=[
-            {"role": "system", "content": "You are a product summarizer, capable of summarizing multiple product reviews into a single concise summary of what the reviews emphasize. Individual product reviews are separated by newline characters."},
-            {"role": "user", "content": "Very useful product, I enjoy how easy it is to use and it only takes 10 minutes to get up and running\n While I enjoy the product, it is very expensive and I would not recommend it due to its cost-to-performance\n As others have noted, the product is very expensive, but if you're like me you'll find this saves a lot of time. Only buy if you know you waste time everyday doing this"}
+            {"role": "system", "content": "You are a product summarizer, capable of summarizing multiple product reviews into a single concise summary of what the reviews emphasize. Individual product reviews are separated by the '|' character. Keep your response to 3 sentences or less."},
+            {"role": "user", "content": review_string}
         ]
     ) 
     # Grab the first message's content: what gpt is sending as result
     processed_data = {'message': completion.choices[0].message.content}
     # Return a json string of first message
     return jsonify(processed_data)
-   
+
+def create_user_content(reviews: List[str]) -> str:
+    reviews_as_str = ""
+    used_chars = 0
+    for review in reviews:
+        piped_str = ""
+        used_chars += len(review)
+        if used_chars > MAX_CHARS:
+            break
+        else:
+            piped_str = review[:-1] + "|"
+        reviews_as_str = reviews_as_str + piped_str
+    return reviews_as_str
 
 
 if __name__ == "__main__":
