aa71bf9d023d8239d0f91599a843c9da7e44717e
Aaron David <amdavid@csuchico.edu>
2024-02-28 12:08:06 -0800

    set debug to false

--- backend/main.py
+++ backend/main.py
@@ -64,4 +64,4 @@ def create_user_content(reviews: List[str]) -> str:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
